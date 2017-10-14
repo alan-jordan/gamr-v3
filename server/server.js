44744581b1dc773cd763ba3919e489578c9ceb53
Alan Jordan <alanpjordan@gmail.com>
2017-10-14 15:27:57 +1300

    First commit.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  credentials: true,
+};
+
+const app = express();
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+module.exports = app;
